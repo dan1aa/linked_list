8689624c82189bdad7222e7f3ae2dde577fab892
dan1aa <burmeidan@gmail.com>
2022-07-21 11:01:17 +0300

    first

---
+++ main.js
@@ -0,0 +1,104 @@
+class linkedListNode {
+    constructor(value, next = null) {
+        this.value = value;
+        this.next = next;
+    }
+
+    toString() {
+        return `${this.value}`;
+    }
+}
+
+class linkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    append(value) {
+        const newNode = new linkedListNode(value);
+
+        if (!this.head || !this.tail) {
+            this.head = newNode;
+            this.tail = newNode;
+
+            return this;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+
+        return this;
+    }
+
+    prepend(value) {
+        const newNode = new linkedListNode(value, this.head);
+        this.head = newNode;
+
+        if (!this.tail) {
+            this.tail = newNode;
+        }
+
+        return this;
+    }
+
+    find(value) {
+        if (!this.head) return null;
+
+        let currentNode = this.head;
+        while (currentNode) {
+            if (currentNode.value === value) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+
+        return null;
+    }
+
+    delete(value) {
+        if (!this.head) return null;
+
+        let deletedNode = null;
+        while (this.head && this.head.value === value) {
+            deletedNode = this.head;
+
+            this.head = this.head.next;
+        }
+
+        let currentNode = this.head;
+
+        if (currentNode !== null) {
+            while (currentNode.next) {
+                if (currentNode.next.value === value) {
+                    deletedNode = currentNode.next;
+                    currentNode.next = currentNode.next.next;
+                } else {
+                    currentNode = currentNode.next;
+                }
+            }
+        }
+
+        if (this.tail?.value === value) {
+            this.tail = currentNode;
+        }
+
+        return deletedNode;
+    }
+
+    toArray() {
+        const nodes = [];
+        let currentNode = this.head;
+
+        while (currentNode) {
+            nodes.push(currentNode);
+            currentNode = currentNode.next;
+        }
+
+        return nodes;
+    }
+
+    toString() {
+        return this.toArray().map((node) => node.toString());
+    }
+}
